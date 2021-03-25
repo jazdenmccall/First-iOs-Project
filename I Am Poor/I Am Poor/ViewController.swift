daa6b9deaf02d39a1d0e775798f7d72bb0394a84
jazdenmccall <mccall.jazden0@gmail.com>
2021-03-25 14:21:38 -0500

    Add files via upload
    First Commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jazden Mccall on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
